f7aa8636838c259bf4a4d423b7faad7412dae906
Divyamani <divyamani9453@gmail.com>
2026-07-29 19:36:51 +0530

    Update login API endpoint to use external URL

--- public/login.js
+++ public/login.js
@@ -6,7 +6,7 @@ form.addEventListener('submit', async (e) => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch("/login", {
+    const response = await fetch("https://hum-aur-hindustan-newspaper.onrender.com/login", {
         method: "POST",
         headers: {
             "Content-type": "application/json"
