7c055948a1cffa4942adab774d9b2df36fca1701
Divyamani <divyamani9453@gmail.com>
2026-07-29 19:41:00 +0530

    update register api endpoint

--- public/register.js
+++ public/register.js
@@ -7,7 +7,7 @@ form.addEventListener('submit', async (e) => {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const response = await fetch('/register', {
+    const response = await fetch('https://hum-aur-hindustan-newspaper.onrender.com/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
